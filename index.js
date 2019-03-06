1e67e0326e6278227c12e575b7567d4eca7aadb6
gregmorihovitis <gregmorihovitis@gmail.com>
2019-03-06 15:07:17 +0000

    dumb typo

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports = props => "This is to go even further beyond... Super Saiyan ${props.level}";
+module.export = props => "This is to go even further beyond... Super Saiyan ${props.level}";
